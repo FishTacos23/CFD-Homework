84c9592e84708d8bc2627113cd9980208b6d77ff
FishTacos23 <32417793+FishTacos23@users.noreply.github.com>
2018-02-03 17:56:48 -0700

    cleaned code

--- Controller.py
+++ Controller.py
@@ -134,19 +134,42 @@ def solve(cv_b, bc_list, p, p_map):
     return t, x
 
 
-if __name__ == '__main__':
+def convergence_study(num_cv_list, x_dist_max, bc_list, p):
+
+    max_temp = []
+
+    def get_temps(num):
+
+        cv_boundaries = [it * x_dist_max / float(num) for it in xrange(num + 1)]  # x boundary locations
+        node_properties = [0 if y <= 0.030000000001 else 1 for y in cv_boundaries]
+        node_properties.append(1)
+
+        temps, x_dist = solve(cv_boundaries, bc_list, p, node_properties)
+        max_temp.append(temps.max())
+        print max_temp[-1]
+
+    get_temps(num_cv_list[-1])
+    num_cv_list.append(num_cv_list[-1]*2)
+    get_temps(num_cv_list[-1])
+
+    while abs(max_temp[-1]-max_temp[-2]) > 0.001:
+
+        num_cv_list.append(num_cv_list[-1]*2)
+        print num_cv_list[-1]
+        get_temps(num_cv_list[-1])
+
+    plt.plot(num_cv_list, max_temp)
+    plt.show()
+
+
+def prob4():
 
     # properties dictionary
     prop1 = {'k': lambda x: 15, 'q': 4000000}
-    # prop2 = {'k': lambda x: 60, 'q': 0}
-    prop2 = {'k': lambda x: 137 * math.exp(25 * x - 2), 'q': 0}
+    prop2 = {'k': lambda x: 60, 'q': 0}
 
     # This variable contains a set of properties for any given segment
     properties = [prop1, prop2]
-
-    # user defined boundary locations
-    # cv_boundaries = [0, .01, .02, .03, .04, .05]
-    # node_properties = [0, 0, 0, 0, 1, 1, 1]             # map cv to properties list
 
     # use for equally spaced control volumes
     num_cvs = 20
@@ -164,3 +187,55 @@ if __name__ == '__main__':
 
     plt.plot(px, py)
     plt.show()
+
+
+def prob5ab():
+    # properties dictionary
+    prop1 = {'k': lambda x: 15, 'q': 4000000}
+    prop2 = {'k': lambda x: 137 * math.exp(25 * x - 2), 'q': 0}
+
+    # This variable contains a set of properties for any given segment
+    properties = [prop1, prop2]
+
+    # use for equally spaced control volumes
+    num_cvs = 20
+    x_max = 0.05
+    cv_boundaries = [it * x_max / float(num_cvs) for it in xrange(num_cvs + 1)]  # x boundary locations
+    node_properties = [0 if y <= 0.030000000001 else 1 for y in cv_boundaries]
+    node_properties.append(1)
+
+    # This variable contains the boundary conditions
+    bound_i = {'type': 'insulated'}
+    bound_c = {'type': 'convective', 'h': 1000, 'To': 300}
+    boundaries = [bound_i, bound_c]
+
+    py, px = solve(cv_boundaries, boundaries, properties, node_properties)
+
+    plt.plot(px, py)
+    plt.show()
+
+
+def prob5c():
+
+    # properties dictionary
+    prop1 = {'k': lambda x: 15, 'q': 4000000}
+    prop2 = {'k': lambda x: 137 * math.exp(25 * x - 2), 'q': 0}
+
+    # This variable contains a set of properties for any given segment
+    properties = [prop1, prop2]
+
+    x_max = 0.05
+
+    # This variable contains the boundary conditions
+    bound_i = {'type': 'insulated'}
+    bound_c = {'type': 'convective', 'h': 1000, 'To': 300}
+    boundaries = [bound_i, bound_c]
+
+    # for grid convergence study
+    number_control_volumes = [5]
+    convergence_study(number_control_volumes, x_max, boundaries, properties)
+
+
+if __name__ == '__main__':
+
+    prob5ab()
--- p3.py
+++ p3.py
@@ -24,7 +24,7 @@ def solve():
     a_mat[0, 1] = -2*k_a/dx
 
     # Node 2
-    x.append(x[-1] + dx/2)
+    x.append(x[-1] + dx / 2)
     a_mat[1, 0] = -2*k_a/dx
     a_mat[1, 1] = k_a / dx + 2*k_a/dx
     a_mat[1, 2] = -k_a/dx
@@ -43,30 +43,30 @@ def solve():
     a_mat[3, 2] = -k_a / dx
     a_mat[3, 3] = k_h/dx + k_a/dx
     a_mat[3, 4] = -k_h/dx
-    b_vec[2] = q_a * bdx
+    b_vec[3] = q_a * bdx
 
     # Node 5 - East of Surface
     x.append(x[-1] + dx)
     k_h = math.pow(((.5 / k_a) + (.5 / k_b(x[-1]))), -1)
     a_mat[4, 3] = -k_h / dx
     k_h = math.pow(((.5 / k_b(x[-1])) + (.5 / k_b(x[-1]+dx))), -1)
-    a_mat[4, 5] = -k_h / dx
+    a_mat[4, 5] = -k_h/dx
     a_mat[4, 4] = -a_mat[4, 3] - a_mat[4, 5]
 
     # Node 6
     x.append(x[-1] + dx)
     k_h = math.pow(((.5 / k_b(x[-1]-dx)) + (.5 / k_b(x[-1]))), -1)
     a_mat[5, 4] = -k_h / dx
-    k_h = (dx/2)*k_b(x[-1]+dx/2)*k_b(x[-1])/(k_b(x[-1]+dx/2)*dx/2)
+    k_h = dx*k_b(x[-1]+dx/2)*k_b(x[-1])/(k_b(x[-1]+dx/2)*dx/2)
     a_mat[5, 6] = -2 * k_h / dx
     a_mat[5, 5] = -a_mat[5, 4] - a_mat[5, 6]
 
     # Node 7 - RB
     x.append(x[-1] + dx/2)
-    k_h = (dx/2) * k_b(x[-1] - dx / 2) * k_b(x[-1]) / (k_b(x[-1]) * dx / 2)
+    k_h = dx * k_b(x[-1] - dx / 2) * k_b(x[-1]) / (k_b(x[-1]) * dx / 2)
     a_mat[6, 5] = -2 * k_h / dx
-    a_mat[6, 6] = h - a_mat[6, 5]
-    b_vec[2] = h*T_o
+    a_mat[6, 6] = h - a_mat[6,5]
+    b_vec[6] = h*T_o
 
     a_mat = np.matrix(a_mat)
     b_vec = np.matrix(b_vec)
@@ -74,6 +74,8 @@ def solve():
     t = a_mat.I*b_vec
 
     t = np.asarray(t)
+
+    print t
 
     plt.plot(x, t)
     plt.show()
