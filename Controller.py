65ab0e320522adefbcee6f48090add99381f0693
FishTacos23 <32417793+FishTacos23@users.noreply.github.com>
2018-02-14 10:33:00 -0700

    homework 3 problem 1

--- Controller.py
+++ Controller.py
@@ -5,9 +5,50 @@ import solver
 import p3
 
 
+def exact_study(num_cv_list, x_dist_max, bc_list, p, exact, max_cv):
+
+    max_temp = []
+
+    def get_temps(num):
+
+        cv_boundaries = [it * x_dist_max / float(num) for it in xrange(num + 1)]  # x boundary locations
+        node_properties = [0 if y <= 0.030000000001 else 1 for y in cv_boundaries]
+        node_properties.append(1)
+
+        temps, x_dist = solver.solve(cv_boundaries, bc_list, p, node_properties)
+        max_temp.append(temps.max())
+
+    get_temps(num_cv_list[-1])
+    num_cv_list.append(num_cv_list[-1]*2)
+    get_temps(num_cv_list[-1])
+
+    while num_cv_list[-1] < max_cv:
+
+        num_cv_list.append(num_cv_list[-1]*2)
+        get_temps(num_cv_list[-1])
+
+    print num_cv_list
+    print max_temp
+
+    diff_temps = []
+    exact_x = []
+    exact_y = []
+
+    for i in xrange(len(max_temp)):
+        diff_temps.append((exact-max_temp[i])/exact)
+        exact_x.append(num_cv_list[i])
+        exact_y.append(exact)
+    print diff_temps
+
+    plt.semilogx(exact_x, exact_y, 'r--')
+    plt.semilogx(num_cv_list, max_temp)
+    plt.show()
+
+
 def convergence_study(num_cv_list, x_dist_max, bc_list, p, crit):
 
     max_temp = []
+    exact = 588.117139215064
 
     def get_temps(num):
 
@@ -112,6 +153,30 @@ def prob5c():
     convergence_study(number_control_volumes, x_max, boundaries, properties, crt)
 
 
+def prob1():
+
+    exact = 588.117139215064
+    max_cv = 10240
+
+    # properties dictionary
+    prop1 = {'k': lambda x: 15, 'q': 4000000}
+    prop2 = {'k': lambda x: 137 * math.exp(25 * x - 2), 'q': 0}
+
+    # This variable contains a set of properties for any given segment
+    properties = [prop1, prop2]
+
+    x_max = 0.05
+
+    # This variable contains the boundary conditions
+    bound_i = {'type': 'insulated'}
+    bound_c = {'type': 'convective', 'h': 1000, 'To': 300}
+    boundaries = [bound_i, bound_c]
+
+    # for grid convergence study
+    number_control_volumes = [5]
+    exact_study(number_control_volumes, x_max, boundaries, properties, exact, max_cv)
+
+
 if __name__ == '__main__':
 
-    prob5c()
+    prob1()
