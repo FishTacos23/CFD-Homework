u=1 works, others give wrong results

Controller.py
@@ -1,114 +1,67 @@
-# This is a 1D CFD Thermal Solver using Practice B
+# This is a 1D CFD Thermal Solver using Practice A
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 import solver
 
 
-def converged(temperature1, temperature2, method, p, x):
+def prob_1_3():
 
-    if method == 'max_diff':
-        return np.absolute(temperature1 - temperature2).max()
-    elif method == 'heat_flux_l23':
-        q1 = p['k'](x[-2])*(temperature1[-2]-temperature1[-3])/(x[-2]-x[-3])
-        q2 = p['k'](x[-2])*(temperature2[-2]-temperature2[-3])/(x[-2]-x[-3])
-        return math.fabs(q1-q2)
-    elif method == 'heat_flux_f0':
-        q1 = p['k'](x[1]) * (temperature1[0] - temperature1[1]) / (x[1] - x[0])
-        q2 = p['k'](x[1]) * (temperature2[0] - temperature2[1]) / (x[1] - x[0])
-        return math.fabs(q1 - q2)
+    """
+    :return: Prints a table for values of phi, and error for different schemes and velocities, and plots of phi
+    """
 
+    schemes = ['central', 'upwind', 'hybrid', 'power_law']
+    markers = ['o', '*', '+', 'v']
+    us = [1., 20., 75.]
 
-def init_guess(method, x, t):
+    boundaries = [1., 0.]
+    rho = 1.
+    gamma = 1.
+    length = 1.
+    x = np.linspace(0., length, 21)
 
-    if method == 'constant':
-        return np.ones(len(x), dtype=float) * t
-    elif method == 'analytical':
-        exact = []
-        n = math.sqrt(4. * t[2] / (t[3](0.) * t[4]))
-        for x_v in x:
-            t_act = (t[0] - t[1]) * math.cosh(n * (x[-1] - x_v)) / math.cosh(n * x[-1]) + t[1]
-            exact.append(t_act)
-        return np.asarray(exact)
-
-
-def hw4_iterator(prop1, alpha, c_method, g_method, g_value):
-
-    num_cv_list = [10, 20, 40, 80]
-
-    tolerance = 0.000001
-
-    # geometry basics
-    x_max = 0.02
-
-    # This variable contains a set of properties for any given segment
-    properties = [prop1]
-
-    # This variable contains the boundary conditions
-    bound_i = {'type': 'fixed', 'T': 400.}
-    bound_c = {'type': None}
-    boundaries = [bound_i, bound_c]
-
-    for num_cv in num_cv_list:
-
-        cv_boundaries = [it * x_max / float(num_cv) for it in xrange(num_cv + 1)]
-        node_properties = [0] * (num_cv + 1)
-        node_properties.append(0)
-
-        x = solver.assemble_geometry(cv_boundaries)
-
-        criterion = 1
-        temperature_prev = init_guess(g_method, x, g_value)
-        temp_totals = []
-
-        counter = 0
-        x_pos = None
-
-        # iterate until temperature convergence is reached
-        while criterion > tolerance:
-            # calculate temperatures
-            temperature_current, x_pos = solver.solve(cv_boundaries, boundaries, properties, node_properties,
-                                                      temperature_prev, method='gs', alpha=alpha)
-
-            criterion = converged(temperature_current, temperature_prev, c_method, prop1, x_pos)
-
-            # set previous
-            temperature_prev = temperature_current
-            temp_totals.append(temperature_prev)
-
-            counter += 1
-        print counter
-
-        # for t in temp_totals:
-        #     plt.plot(x_pos, t)
-        # plt.show()
+    for u in us:
+        print u
+        phi_exact = analytical(x, rho, u, length, gamma)
+        plt.figure()
+        plt.plot(x, phi_exact)
+        for i in xrange(len(schemes)):
+            print schemes[i]
+            f = rho / u
+            phi_nm = solver.solve(x, boundaries, schemes[i], f, gamma)
+            plt.plot(x, phi_nm, markers[i])
+            err = 0
+            for i in xrange(phi_nm.shape[0]):
+                err += math.fabs(phi_nm[i]-phi_exact[i])
+            print 'Phi Values:'
+            print phi_nm
+            print 'error = ' + str(err)
+    plt.show()
 
 
-def prob1():
-    prop1 = {'k': lambda x: 401., 'h': 10., 'e': 0, 'T': 273., 'D': 0.003}
-    c_method = 'heat_flux_f0'
-    alpha = 1.
-    hw4_iterator(prop1, alpha, c_method, 'constant', 400.)
+def analytical(x, rho, u, l, gamma):
+    """
 
+    :param x: Position of nodes
+    :type x: ndarray
+    :param rho: Density
+    :type rho: float
+    :param u: Velocity
+    :type u: float
+    :param l: Domain Length
+    :type l: float
+    :param gamma:
+    :type gamma: float
+    :return: temperatures for nodes using the analytical solution
+    """
 
-def prob2():
-    prop1 = {'k': lambda x: 401., 'h': 10., 'e': 1., 'T': 273., 'D': 0.003}
-    c_method = 'heat_flux_f0'
-    alpha_list = [1., 1.2]
+    p = rho * u * l / gamma
 
-    for alpha in alpha_list:
-        hw4_iterator(prop1, alpha, c_method, 'constant', 400.)
+    soln = 1. - (np.exp(p * x / l) - 1.) / (math.exp(p) - 1.)
 
-
-def prob3():
-
-    prop1 = {'k': lambda x: 401., 'h': 10., 'e': 1., 'T': 273., 'D': 0.003}
-    c_method = 'heat_flux_f0'
-    alpha_list = [1., 1.2]
-
-    for alpha in alpha_list:
-        hw4_iterator(prop1, alpha, c_method, 'analytical', [400., 273., prop1['h'], prop1['k'], prop1['D']])
+    return soln
 
 
 if __name__ == '__main__':
-    prob3()
+    prob_1_3()

solver.py
@@ -2,169 +2,71 @@ import numpy as np
 import math
 
 
-def assemble_geometry(cv_b):
-    """
-    This function handles all geometry set up
-    :return: x_nodes, x_bounds
-    """
-
-    # get nodal locations
-    x = [cv_b[0]]
-    for i in xrange(len(cv_b)-1):
-        x.append((cv_b[i]+cv_b[i+1])/2)
-    x.append(cv_b[-1])
-
-    return x
-
-
-def assemble_cv_values(dx_ww, dx_we, dx_ew, dx_ee, bdx, kp, kw, ke, s, bc):
+def assemble_cv_values(x, i, bc_list, gamma, f, scheme):
     """
     This function calculates all the necessary values for a given cv
     :return: Ap, Aw, Ae, b
     """
 
-    # west node
-    if dx_ww == 0 and dx_we == 0:
-        aw = 0
-    else:
-        khw = (dx_ww + dx_we) * kw * kp / (kw * dx_we + kp * dx_ww)
-        aw = khw / (dx_ww + dx_we)
-
-    # east node
-    if dx_ew == 0 and dx_ee == 0:
-        ae = 0
-    else:
-        khe = (dx_ew + dx_ee) * ke * kp / (ke * dx_ew + kp * dx_ee)
-        ae = khe / (dx_ew + dx_ee)
-
-    ap = aw + ae
-    b = s * bdx
+    dx_ww, dx_we, dx_ew, dx_ee, bc = get_cv_geometry(x, i, bc_list)
 
     if bc is not None:
-        if bc['type'] == 'convective':
-            ap += bc['h']
-            b += bc['h']*bc['To']
-        if bc['type'] == 'fixed':
-            ap = 1
-            aw = 0
-            ae = 0
-            b = bc['T']
+
+        ap = 1
+        aw = 0
+        ae = 0
+        b = bc
+
+    else:
+
+        dw = gamma / (dx_we + dx_ww)
+        aw = dw*func_a(scheme, f/dw) + max(f, 0)
+
+        de = gamma / (dx_ee + dx_ew)
+        ae = de*func_a(scheme, f/de) + max(-f, 0)
+
+        ap = aw + ae
+        b = 0.
 
     return ap, aw, ae, b
 
 
-def get_cv_geometry(x, cv_b, i, p, p_map, bc_list, t_prev):
-
-    kp = p[p_map[i]]['k'](x[i])
-
-    if t_prev is not None:
-        boltz = 0.0000000567
-        ts = t_prev[i]
-        tinf = p[p_map[i]]['T']
-
-        sp = (p[p_map[i]]['h'] + 4. * boltz * p[p_map[i]]['e'] * math.pow(ts, 3.)) * 4. / p[p_map[i]]['D']
-
-        sc = (4./p[p_map[i]]['D']) * (p[p_map[i]]['h']*(ts-tinf) +
-                                      boltz*p[p_map[i]]['e']*(math.pow(ts, 4.) - math.pow(tinf, 4.))) - sp*ts
-
-        s = -(sc + sp*ts)
-    else:
-        s = p[p_map[i]]['q']
+def get_cv_geometry(x, i, bc_list):
 
     if i == 0:
         bc = bc_list[0]
 
-        bdx = 0
+        bdx = (x[i+1]-x[i])/2
 
         dx_ww = 0
         dx_we = 0
-        dx_ew = 0
-        dx_ee = x[i + 1] - x[i] - bdx / 2
-
-        kw = 0
-        ke = p[p_map[i + 1]]['k'](x[i+1])
+        dx_ew = bdx
+        dx_ee = x[i + 1] - x[i] - bdx
 
     elif i == len(x)-1:
         bc = bc_list[1]
 
-        bdx = 0
+        bdx = (x[i]-x[i-1])/2
 
-        dx_ww = x[i] - x[i-1] - bdx / 2
-        dx_we = 0
+        dx_ww = x[i] - x[i-1] - bdx
+        dx_we = bdx
         dx_ew = 0
         dx_ee = 0
-
-        kw = p[p_map[i - 1]]['k'](x[i-1])
-        ke = 0
 
     else:
         bc = None
 
-        bdx = (cv_b[i] - cv_b[i - 1])
+        bdx = (x[i] - x[i - 1])
 
         dx_ww = x[i] - x[i-1] - bdx / 2
         dx_we = x[i] - x[i-1] - dx_ww
         dx_ee = x[i+1] - x[i] - bdx / 2
         dx_ew = x[i+1] - x[i] - dx_ee
 
-        kw = p[p_map[i-1]]['k'](x[i-1])
-        ke = p[p_map[i+1]]['k'](x[i+1])
-
-    return [dx_ww, dx_we, dx_ew, dx_ee, bdx, kp, kw, ke, s, bc]
+    return [dx_ww, dx_we, dx_ew, dx_ee, bc]
 
 
-def tdma(x, cv_b, bc_list, p, p_map, t_prev):
-
-    tdma_p = np.empty(len(x), dtype=float)
-    tdma_q = np.empty(len(x), dtype=float)
-    t = np.copy(t_prev)
-
-    # loop over each node to assemble tdma variables
-    for i in xrange(len(x)):
-
-        # get cv values
-        cv_input = get_cv_geometry(x, cv_b, i, p, p_map, bc_list, t)
-        ap, aw, ae, bp = assemble_cv_values(*cv_input)
-
-        # assemble tdma variables
-        if aw == 0:
-            tdma_p[i] = ae / ap
-            tdma_q[i] = bp / ap
-        else:
-            tdma_p[i] = ae / (ap - aw * tdma_p[i - 1])
-            tdma_q[i] = (bp + aw * tdma_q[i - 1]) / (ap - aw * tdma_p[i - 1])
-
-    # loop backwards over each node to solve tdma temperatures
-    for i in xrange(len(x) - 1, 0, -1):
-
-        if i == len(x) - 1:
-            t[i] = tdma_q[i]
-        else:
-            t[i] = tdma_p[i] * t[i + 1] + tdma_q[i]
-
-    return t
-
-
-def g_s(x, cv_b, bc_list, p, p_map,  t_prev, alpha):
-
-    t = np.copy(t_prev)
-    for i in xrange(len(x)):
-
-        # get cv values
-        cv_input = get_cv_geometry(x, cv_b, i, p, p_map, bc_list, t)
-        ap, aw, ae, bp = assemble_cv_values(*cv_input)
-
-        if i == 0:
-            t[i] = t[i] + alpha * ((ae * t[i + 1] + bp) / ap - t[i])
-        elif i == len(x)-1:
-            t[i] = t[i] + alpha * ((aw * t[i - 1] + bp) / ap - t[i])
-        else:
-            t[i] = t[i] + alpha * ((aw * t[i - 1] + ae * t[i + 1] + bp) / ap - t[i])
-
-    return t
-
-
-def mat(x, cv_b, p, p_map, bc_list):
+def mat(x, bc_list, gamma, f, scheme):
 
     a = np.zeros((len(x), len(x)), dtype=float)
     b = np.zeros((len(x), 1), dtype=float)
@@ -173,8 +75,7 @@ def mat(x, cv_b, p, p_map, bc_list):
     for i in xrange(len(x)):
 
         # get cv values
-        cv_input = get_cv_geometry(x, cv_b, i, p, p_map, bc_list, None)
-        ap, aw, ae, bp = assemble_cv_values(*cv_input)
+        ap, aw, ae, bp = assemble_cv_values(x, i, bc_list, gamma, f, scheme)
 
         # assemble into mat
         a[i, i] = ap
@@ -192,20 +93,37 @@ def mat(x, cv_b, p, p_map, bc_list):
     return a.I * b
 
 
-def solve(cv_b, bc_list, p, p_map, t_prev, method=None, alpha=1):
+def func_a(scheme, p):
+    if scheme == 'central':
+        a = 1. - .5*math.fabs(p)
+    elif scheme == 'upwind':
+        a = 1.
+    elif scheme == 'hybrid':
+        a = max(0., 1. - .5*math.fabs(p))
+    elif scheme == 'power_law':
+        a = max(0., math.pow(1.-.1*math.fabs(p), 5.))
+    else:
+        raise ValueError("I'm sorry Dave, I can't do that.")
+    return a
+
+
+def solve(x, bc_list, scheme, f, gamma):
     """
     This function assembles A and b then solves for T (AT = b)
+    :param x: location of the nodes
+    :type x: ndarray
+    :param bc_list: [boundary condition 1, boundary condition 2]
+    :type bc_list: [dict, dict]
+    :param scheme: convection / diffusion calculation scheme
+    :type scheme: str
+    :param f: convective mass per unit area
+    :type f: float
+    :param gamma:
+    :type gamma: float
     :return: iterable, T
     """
 
     # get geometry
-    x = assemble_geometry(cv_b)
+    t = mat(x, bc_list, gamma, f, scheme)
 
-    if method == 'tdma':
-        t = tdma(x, cv_b, bc_list, p, p_map, t_prev)
-    elif method == 'gs':
-        t = g_s(x, cv_b, bc_list, p, p_map,  t_prev, alpha)
-    else:
-        t = mat(x, cv_b, p, p_map, bc_list)
-
-    return t, x
+    return np.asarray(t)